50b80e44cbfdee6b8ad6c34699c52c8b2a035de4
Karoline Suzuki <42789906+suzukikarol@users.noreply.github.com>
2019-06-11 17:28:59 -0300

    first commit

---
+++ WORK-FC/Acoes.js
@@ -0,0 +1,18 @@
+{/* <script>
+    function mudarNome()
+{
+ if(document.getElementById("button").value == "Ativo")
+ {
+        document.getElementById("button").value = "Inativo";
+    }
+    else
+ {
+        document.getElementById("button").value = "Ativo";
+    }
+   } */}
+// </script>
+// <body>
+// <input type="button" id="button" onclick="mudarNome();" value="Ativo" />
+// </body>
+
+feather.replace()
